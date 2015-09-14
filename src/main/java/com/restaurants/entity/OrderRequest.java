003c2fc29404fa5d41c1fe680d9e199b1e6f4f86
hukuuu <hukuuu@gmail.com>
2015-09-14 22:44:45 +0300

    added time and seats to order request

--- src/main/java/com/restaurants/entity/OrderRequest.java
+++ src/main/java/com/restaurants/entity/OrderRequest.java
@@ -1,25 +1,33 @@
 package com.restaurants.entity;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
 @SuppressWarnings("serial")
 @Entity
-@Where(clause="DELETED != 'TRUE'")
+@Where(clause = "DELETED != 'TRUE'")
 @SQLDelete(sql = "UPDATE OrderRequest SET DELETED = 'TRUE' WHERE id=?")
 public class OrderRequest extends BaseEntity {
-	
+
 	@ManyToOne
 	private Restaurant restaurant;
-	
+
 	@OneToMany
 	private List<Food> foods;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date time;
+
+	private Integer seats;
 
 	public Restaurant getRestaurant() {
 		return restaurant;
@@ -36,7 +44,21 @@ public class OrderRequest extends BaseEntity {
 	public void setFoods(List<Food> foods) {
 		this.foods = foods;
 	}
-	
-	
+
+	public Date getTime() {
+		return time;
+	}
+
+	public void setTime(Date time) {
+		this.time = time;
+	}
+
+	public Integer getSeats() {
+		return seats;
+	}
+
+	public void setSeats(Integer seats) {
+		this.seats = seats;
+	}
 
 }
